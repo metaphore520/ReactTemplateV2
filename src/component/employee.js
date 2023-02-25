f4ea2db41dd57325f8c4ea930853bf254af7b1ea
metaphore520 <119277137+metaphore520@users.noreply.github.com>
2023-02-25 15:55:01 +0600

    Post Data Adde

--- src/component/employee.js
+++ src/component/employee.js
@@ -4,6 +4,17 @@ import { API } from "../Variable";
 class Employee extends Component {
   state = {
     countries: [],
+    countryToAdd: {
+      Name: "Place...",
+      CountryCode: 0,
+    },
+  };
+
+  NameChanged = (value) => {
+    this.setState({ countryToAdd: { ...this.state.countryToAdd, Name: value } });
+  };
+  CountryCodeChanged = (value) => {
+    this.setState({ countryToAdd: { ...this.state.countryToAdd,CountryCode: value } });
   };
 
   refreshList() {
@@ -17,30 +28,84 @@ class Employee extends Component {
     //   console.log(response)
     // );
   }
+  createStudent = () => {
+    fetch(API.API_URL + API.postCountry, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+       this.state.countryToAdd,
+      ),
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          alert(result);
+          this.refreshList();
+        },
+        (error) => {
+          console.log(error);
+          alert("Failed");
+        }
+      );
+  };
+
   componentDidMount() {
     this.refreshList();
   }
   render() {
     const { countries } = this.state;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Country Name</th>
-            <th>Country Code</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countries.map((data) => (
+      <>
+        <div className="row">
+          <div className="col-md-2">
+            <label>Name : </label>
+          </div>
+          <div className="col-md-2">
+            <input
+              type="text"
+              value={this.state.countryToAdd.Name}
+              onChange={(e) => this.NameChanged(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-2">
+            <label>Country Code : </label>
+          </div>
+          <div className="col-md-2">
+            <input
+              type="text"
+              value={this.state.countryToAdd.CountryCode}
+              onChange={(e) => this.CountryCodeChanged(e.target.value)}
+            />
+          </div>
+          <div className="col-md-2">
+            <button onClick={() => this.createStudent()}>Save Student</button>
+          </div>
+        </div>
+
+        <table>
+          <thead>
             <tr>
-              <td>{data.Id}</td>
-              <td>{data.Name}</td>
-              <td>{data.CountryCode}</td>
+              <th>Id</th>
+              <th>Country Name</th>
+              <th>Country Code</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {countries.map((data) => (
+              <tr key={data.Id}>
+                <td>{data.Id}</td>
+                <td>{data.Name}</td>
+                <td>{data.CountryCode}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
